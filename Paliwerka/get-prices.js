5d3924ddecb7c431af3976e987b8bf2f3019eb02
lukgru <lukasz.gruchala@fitech.pl>
2023-01-20 12:18:17 +0100

    working on scrapping price

--- Paliwerka/get-prices.js
+++ Paliwerka/get-prices.js
@@ -1,44 +1,48 @@
-const { playwright, chromium } = require('playwright');
-const data = [];
+const playwright = require('playwright');
 
-    async function getPrices() {
-        var browser = await chromium.launch();
+(async () => {
+    const browser = await playwright.chromium.launch();
 
-        var page = await browser.newPage();
+    const page = await browser.newPage();
 
-        await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
+    await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
 
-        await page.goto('https://www.autocentrum.pl/stacje-paliw/');
+    await page.goto('https://www.autocentrum.pl/stacje-paliw/');
 
-        await page.getByRole('button', { name: 'AKCEPTUJĘ I PRZECHODZĘ DO SERWISU'}).click();
+    await page.getByRole('button', { name: 'AKCEPTUJĘ I PRZECHODZĘ DO SERWISU' }).click();
 
-        await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
+    await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
 
-        await page.getByPlaceholder('Wpisz nazwę lub adres stacji').click();
+    await page.getByPlaceholder('Wpisz nazwę lub adres stacji').click();
 
-        await page.keyboard.insertText('Kraków');
+    await page.keyboard.insertText('Kraków');
 
-        await page.keyboard.press('Enter');
+    await page.keyboard.press('Enter');
 
-        await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
+    await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
 
-        await page.locator('form').filter({ hasText: 'Szukaj' }).getByRole('button', { name: '' });
+    await page.locator('form').filter({ hasText: 'Szukaj' }).getByRole('button', { name: '' });
 
-        await page.getByRole('combobox').selectOption('lpg');
+    await page.getByRole('combobox').selectOption('lpg');
 
-        const prices = await page.$$eval('.station-item', wholeData => {
-            wholeData.forEach(price => {
-                const stationName = price.querySelector('.address').innerText;
-                const address = price.querySelector('.name.shorter').innerText;
-                const lpgPrice = price.querySelector('.petrol.lpg').innerText;
-                const lastUpdate = price.querySelector('.petrol.lpg').innerHTML;
-                data.push({ stationName, address, lpgPrice, lastUpdate });
-            })
+    await page.waitForLoadState();
 
-            return data;
-        });
+    const prices = await page.$$eval('.station-item', wholeData => {
+        const data = [];
+        wholeData.forEach(price => {
+            const stationName = price.querySelector('.address').innerText;
+            const address = price.querySelector('.name').innerText;
+            // const lpgPrice = price.querySelector('.petrols-wrapper .lpg').innerText;
+// TODO FIX LOCAOTRS
+            
+            // const lastUpdate = price.querySelector('.lpg').innerHTML;
+            data.push({ stationName, address, lpgPrice });
+        })
+
+        return data;
+    });
     console.log(prices);
-    console.log(prices.length);
-    }   
 
-    getPrices();
+    await browser.close();
+})
+();
