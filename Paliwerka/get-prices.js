e67fbb766401a8191963979efad516c24b151e8a
lukgru <lukasz.gruchala@fitech.pl>
2023-01-22 20:48:31 +0100

    fixed waits

--- Paliwerka/get-prices.js
+++ Paliwerka/get-prices.js
@@ -5,13 +5,11 @@ const playwright = require('playwright');
 
     const page = await browser.newPage();
 
-    await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
-
     await page.goto('https://www.autocentrum.pl/stacje-paliw/');
 
     await page.getByRole('button', { name: 'AKCEPTUJĘ I PRZECHODZĘ DO SERWISU' }).click();
 
-    await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
+    await page.waitForLoadState('load', { timeout: 1000000 });
 
     await page.getByPlaceholder('Wpisz nazwę lub adres stacji').click();
 
@@ -19,19 +17,23 @@ const playwright = require('playwright');
 
     await page.keyboard.press('Enter');
 
-    await page.route('**/*.{png,jpg.jpeg}', route => route.abort());
+    await page.waitForLoadState('networkidle', { timeout: 1000000 });
 
-    await page.locator('form').filter({ hasText: 'Szukaj' }).getByRole('button', { name: '' });
+    await page.waitForTimeout(3000)
+
+    await page.getByRole('combobox').waitFor();
 
     await page.getByRole('combobox').selectOption('lpg');
+
+    await page.waitForLoadState('networkidle', { timeout: 1000000 } )
 
     const prices = await page.$$eval('.station-item', wholeData => {
         const data = [];
         wholeData.forEach(price => {
-            
+
             const petrol = price.getElementsByClassName('petrol lpg');
 
-            
+
             for (var i = 0; i < petrol.length; i++) {
                 const stationName = price.querySelector('.address').innerText;
                 const address = price.querySelector('.name').innerText;
@@ -39,7 +41,7 @@ const playwright = require('playwright');
                 var lastUpdatedOn = petrol[i].getAttribute('title');
                 data.push({ stationName, address, petrolPrice, lastUpdatedOn });
             }
-            
+
         })
         return data;
 
